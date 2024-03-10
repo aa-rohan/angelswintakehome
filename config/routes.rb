e12a5b9b82922545af6b7f14c242f5f5f176dc37
Aarohan <aarohanthapa@bajratechnologies.com>
2024-03-10 14:28:26 +0545

    add routes for contents

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/users/signup', to: 'users#signup'
       post '/users/signin', to: 'users#signin'
+
+      resources :contents, only: %i[create index update destroy]
     end
   end
 end
