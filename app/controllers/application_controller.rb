97c9699c6fc66d98c1b6cd3b150b244db67b4c85
Aarohan <aarohanthapa@bajratechnologies.com>
2024-03-10 15:01:58 +0545

    add authentication and current_user feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request!
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    decoded = JsonWebToken.decode(header)
+    @current_user = User.find(decoded[:user_id])
+  rescue StandardError
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
 end
